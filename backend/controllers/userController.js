9ed0c88d2ea59dc489d338cc7f10b0ed28429526
Samarth Shinde <samarthsshinde@gmail.com>
2024-09-08 09:48:54 +0530

    3. signin and matchPassword

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
 
@@ -5,38 +6,53 @@ import generateToken from "../utils/generateToken.js";
 //route POST  api/users/auth
 //@access Public
 const authUser = async (req, res) => {
-	res.status(200).json({ message: "Auth user" });
+	try {
+		const { email, password } = req.body;
+
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(401).json({ message: "Invalid Credentials" });
+		}
+
+		if (await user.matchPassword(password)) {
+			generateToken(res, user._id);
+
+			return res.status(201).json(user);
+		} else {
+			return res
+				.status(403)
+				.json({ message: "password does not match or try again" });
+		}
+	} catch (error) {
+		console.error(error);
+		return res.status(400).json({ message: "error occured", error });
+	}
 };
 
 //@desc Register a new user
 //route POST  api/users
 //@access Public
-const registerUser = async (req, res) => {
-	try {
-		const { name, email, password } = req.body;
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
 
-		const userExists = await User.findOne({ email: email });
+	const userExists = await User.findOne({ email: email });
 
-		if (userExists) {
-			res.status(400);
-			throw new Error("User already exists");
-		}
-
-		const user = await User.create({ name, email, password });
-
-		if (user) {
-			generateToken(res, user._id);
-
-			res.status(201).json({ user });
-		} else {
-			res.status(400);
-			throw new Error("Invalid user Data");
-		}
-	} catch (error) {
-		console.error(`Error: ${error}`);
-		return res.status(411).json({ message: "error occured" });
+	if (userExists) {
+		res.status(400);
+		throw new Error("User already exists");
 	}
-};
+
+	const user = await User.create({ name, email, password });
+
+	if (user) {
+		generateToken(res, user._id);
+
+		res.status(201).json({ user });
+	} else {
+		res.status(400);
+		throw new Error("Invalid user Data");
+	}
+});
 
 //@desc Logout user
 //route POST  api/users/logout
